1fb355b1a169fd9a73814178e379ac4c4c5e6cd6
Maxim Musienko <mmusienko@codenvy.com>
2015-04-15 16:22:59 +0300

    asd

---
+++ src/main/java/com/codenvy/example/spring/Tst.java
@@ -0,0 +1,10 @@
+package com.codenvy.example.spring;
+
+public class Tst {
+   String someVal(){
+     String testString = "codenvy the best IDE in the world";
+     testString.toUpperCase();
+    return testString;
+  }
+  
+}
